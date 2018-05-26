65c193a1d2c7690f96bb25ab26f8767b1ab8cb17
Supereg <mail@anderl-bauer.de>
2018-05-26 19:18:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# homebridge-http-temperature-sensor
+
+## Configuration
+
+```json
+{
+    "accessories": [
+        {
+          "accessory": "HTTP-TEMPERATURE",
+          "name": "Temperature Sensor",
+          
+          "getUrl": "http://localhost/api/getTemperature"
+        }   
+    ]
+}
+```
+
+* `getUrl` is the url which is used to retrieve the current temperature in celcius. It is called using a **GET** request. 
+It expected the current temperature as an float in a range from `0` to `100` and a maximum step value of `0.1` without 
+any html markup.
+
+## Notification Server
+
+`homebridge-http-temperature-sensor` can be used together with 
+[homebridge-http-notification-server](https://github.com/Supereg/homebridge-http-notification-server) in order to receive
+updates when the state changes at your external program. For details on how to implement those updates and how to 
+install and configure `homebridge-http-notification-server`, please refer to the 
+[README](https://github.com/Supereg/homebridge-http-notification-server) of the repository.
+
+Down here is an example on how to configure `homebridge-http-temperature-sensor` to work with your implementation of the 
+`homebridge-http-notification-server`.
+
+```json
+{
+    "accessories": [
+        {
+          "accessory": "HTTP-TEMPERATURE",
+          "name": "Temperature Sensor",
+          
+          "notificationID": "my-humidity-sensor",
+          "notificationPassword": "superSecretPassword",
+          
+          "getUrl": "http://localhost/api/getTemperature"
+        }   
+    ]
+}
+```
+
+* `notificationID` is an per Homebridge instance unique id which must be included in any http request.  
+* `notificationPassword` is **optional**. It can be used to secure any incoming requests.
+
+To get more details about the configuration have a look at the 
+[README](https://github.com/Supereg/homebridge-http-notification-server).
---
+++ index.js
@@ -0,0 +1,117 @@
+"use strict";
+
+let Service, Characteristic, api;
+
+const request = require("request");
+const packageJSON = require("./package.json");
+
+module.exports = function (homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+
+    api = homebridge;
+
+    homebridge.registerAccessory("homebridge-http-temperature-sensor", "HTTP-TEMPERATURE", HTTP_TEMPERATURE);
+};
+
+function HTTP_TEMPERATURE(log, config) {
+    this.log = log;
+    this.name = config.name;
+
+    this.getUrl = config.getUrl;
+
+    this.homebridgeService = new Service.TemperatureSensor(this.name);
+    this.homebridgeService.getCharacteristic(Characteristic.CurrentTemperature)
+        .on("get", this.getTemperature.bind(this));
+
+    this.notificationID = config.notificationID || this.name.toLowerCase().replace(" ", "-");
+    this.notificationPassword = config.notificationPassword;
+
+    api.on("didFinishLaunching", function () {
+        if (api.notificationRegistration && typeof api.notificationRegistration === "function") {
+            try {
+                api.notificationRegistration(this.notificationID, this.handleNotification.bind(this), this.notificationPassword);
+                this.log("Detected running notification server. Registered successfully!");
+            } catch (error) {
+                this.log("Could not register notification handler. ID '" + this.notificationID + "' is already taken!")
+            }
+        }
+    }.bind(this));
+}
+
+HTTP_TEMPERATURE.prototype = {
+
+    identify: function (callback) {
+        this.log("Identify requested!");
+        callback();
+    },
+
+    getServices: function () {
+        const informationService = new Service.AccessoryInformation();
+
+        informationService
+            .setCharacteristic(Characteristic.Manufacturer, "Andreas Bauer")
+            .setCharacteristic(Characteristic.Model, "HTTP Temperature Sensor")
+            .setCharacteristic(Characteristic.SerialNumber, "TS01")
+            .setCharacteristic(Characteristic.FirmwareRevision, packageJSON.version);
+
+        return [informationService, this.homebridgeService];
+    },
+
+    handleNotification: function(body) {
+        const value = body.value;
+
+        let characteristic;
+        switch (body.characteristic) {
+            case "CurrentTemperature":
+                characteristic = Characteristic.CurrentTemperature;
+                break;
+            default:
+                this.log("Encountered unknown characteristic handling notification: " + body.characteristic);
+                return;
+        }
+
+        this.log("Updating '" + body.characteristic + "' to new value: " + body.value);
+        this.homebridgeService.setCharacteristic(characteristic, value);
+    },
+
+    getTemperature: function (callback) {
+        this._doRequest("getTemperature", this.getUrl, "GET", "getUrl", callback, function (body) {
+            const temperature = parseFloat(body);
+            this.log("temperature is currently at %s", temperature);
+
+            callback(null, temperature);
+        }.bind(this))
+    },
+
+    _doRequest: function (methodName, url, httpMethod, urlName, callback, successCallback) {
+        if (!url) {
+            this.log.warn("Ignoring " + methodName + "() request, '" + urlName + "' is not defined!");
+            callback(new Error("No '" + urlName + "' defined!"));
+            return;
+        }
+
+        request(
+            {
+                url: url,
+                body: "",
+                method: httpMethod,
+                rejectUnauthorized: false
+            },
+            function (error, response, body) {
+                if (error) {
+                    this.log(methodName + "() failed: %s", error.message);
+                    callback(error);
+                }
+                else if (response.statusCode !== 200) {
+                    this.log(methodName + "() returned http error: %s", response.statusCode);
+                    callback(new Error("Got http error code " + response.statusCode));
+                }
+                else {
+                    successCallback(body);
+                }
+            }.bind(this)
+        );
+    }
+
+};
